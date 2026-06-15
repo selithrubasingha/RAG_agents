b35bd59d56c199468b7d023abd99748d830a4c37
Selith <selithrubasingha@gmail.com>
2026-06-15 17:05:41 +0530

    agent 1 : super basic llm agent

---
+++ langgraph_agents/agent1.py
@@ -0,0 +1,28 @@
+from typing import TypedDict  , List
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START , END
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI  # New Import
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7) 
+
+def process(state: AgentState)->AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"Agent Response: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+
+graph.add_node("process",process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+
+agent = graph.compile()
+
+user_input = input("Enter: ")
+agent.invoke({"messages":[HumanMessage(content=user_input)]})
