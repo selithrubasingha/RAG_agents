58d7361b17ba444ca0897f6a97f3099f2c1a47c7
Selith <selithrubasingha@gmail.com>
2026-06-15 14:09:48 +0530

    graph HelloWorld

--- langgraph_agents/main.py
+++ langgraph_agents/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from langgraph-agents!")
+from typing import Dict , TypedDict
+from langgraph.graph import StateGraph
+
+class AgentState(TypedDict): # our state schema 
+    message : str
 
 
-if __name__ == "__main__":
-    main()
+def greeting_node(state : AgentState) -> AgentState:
+    """Simple node that generates a greeting message."""
+
+    state['message'] = "hey ! " +state['message']+ " how are you ?"
+
+    return state
+
+graph = StateGraph(AgentState)
+
+graph.add_node("greeter", greeting_node)
+
+graph.set_entry_point("greeter")
+graph.set_finish_point("greeter")
+
+app = graph.compile()
+
+from IPython.display import Image,display
+
+# --- THE NEW WAY TO GET THE IMAGE ---
+png_bytes = app.get_graph().draw_mermaid_png()
+
+with open("graph.png", "wb") as f:
+    f.write(png_bytes)
+    
+print("Graph saved to graph.png!")
+# ------------------------------------
+result = app.invoke({"message": "Bob"})
+
+print(result["message"])
