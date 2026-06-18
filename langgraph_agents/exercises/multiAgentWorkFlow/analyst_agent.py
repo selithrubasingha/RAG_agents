07e0fc489ae917ff49d0c835b813495a9d8f65d9
Selith <selithrubasingha@gmail.com>
2026-06-18 19:18:00 +0530

    multi agent workflow : anallyst agent

---
+++ langgraph_agents/exercises/multiAgentWorkFlow/analyst_agent.py
@@ -0,0 +1,14 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.prebuilt import create_react_agent
+from tools.sql_query import query_database
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.0)
+
+analyst_tools = [query_database]
+analyst_prompt = "You are the Senior Data Analyst. Query the SQL database to find internal land sales metrics. Do not make up data. If a query fails, adjust your SQL syntax and try again."
+
+compiled_analyst = create_react_agent(
+    model, 
+    tools=analyst_tools, 
+    state_modifier=analyst_prompt
+)
